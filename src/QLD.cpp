09c9ab07954f8010428a237234e5a3f556f03bb6
Pierre Gergondet <pierre.gergondet@gmail.com>
2019-11-29 12:48:31 +0800

    Use GitHub actions (#14)
    * [ci/packaging] Enable GitHub Actions
    
    * [ci/macOS] Install gfortran as a cask dependency
    
    * [ci] Rely on the fact that install-dependencies installs gfortran
    
    Works around very strange bug on 16.04 runner
    
    * [packaging] Correctly check debian/changelog
    
    * [clang-format] Apply clang-format and add it to CI
    
    * [ci] Upload documentation to gh-pages
    
    * [packaging] Use correct repository before pushing to jrl-umi3218
    
    * [README] Update README

--- src/QLD.cpp
+++ src/QLD.cpp
@@ -7,95 +7,69 @@
 
 // includes
 
-
 namespace Eigen
 {
 
-QLD::QLD():
-	A_(),
-	B_(),
-	fdOut_(0),
-	verbose_(false),
-	fail_(0),
-	U_(),
-	WAR_(),
-	IWAR_()
-{ }
+QLD::QLD() : A_(), B_(), fdOut_(0), verbose_(false), fail_(0), U_(), WAR_(), IWAR_() {}
 
-
-QLD::QLD(int nrvar, int nreq, int nrineq, bool verbose):
-	A_(),
-	B_(),
-	fdOut_(0),
-	verbose_(verbose ? 1 : 0),
-	fail_(0),
-	U_(),
-	WAR_(),
-	IWAR_()
+QLD::QLD(int nrvar, int nreq, int nrineq, bool verbose)
+: A_(), B_(), fdOut_(0), verbose_(verbose ? 1 : 0), fail_(0), U_(), WAR_(), IWAR_()
 {
-	problem(nrvar, nreq, nrineq);
+  problem(nrvar, nreq, nrineq);
 }
-
 
 void QLD::fdOut(int fd)
 {
-	fdOut_ = fd;
+  fdOut_ = fd;
 }
-
 
 int QLD::fdOut() const
 {
-	return fdOut_;
+  return fdOut_;
 }
-
 
 void QLD::verbose(bool v)
 {
-	verbose_ = v ? 1 : 0;
+  verbose_ = v ? 1 : 0;
 }
-
 
 bool QLD::verbose() const
 {
-	return verbose_;
+  return verbose_;
 }
-
 
 int QLD::fail() const
 {
-	return fail_;
+  return fail_;
 }
-
 
 void QLD::problem(int nrvar, int nreq, int nrineq)
 {
-	int nrconstr = nreq + nrineq;
+  int nrconstr = nreq + nrineq;
 
-	int MMAX = nrconstr == 0 ? 1 : nrconstr;
-	// nrvar can't be == 0 so NMAX == nrvar
-	int NMAX = nrvar;
+  int MMAX = nrconstr == 0 ? 1 : nrconstr;
+  // nrvar can't be == 0 so NMAX == nrvar
+  int NMAX = nrvar;
 
-	A_.resize(MMAX, NMAX);
-	B_.resize(MMAX);
+  A_.resize(MMAX, NMAX);
+  B_.resize(MMAX);
 
-	X_.resize(nrvar);
+  X_.resize(nrvar);
 
-	U_.resize(nrconstr + 2*nrvar);
-	int LWAR = static_cast<int>(
-		std::ceil((3.*NMAX*NMAX)/2. + 10.*NMAX + MMAX + nrconstr + 1.));
-	WAR_.resize(LWAR);
-	IWAR_.resize(nrvar);
+  U_.resize(nrconstr + 2 * nrvar);
+  int LWAR = static_cast<int>(std::ceil((3. * NMAX * NMAX) / 2. + 10. * NMAX + MMAX + nrconstr + 1.));
+  WAR_.resize(LWAR);
+  IWAR_.resize(nrvar);
 }
 
-
-const VectorXd& QLD::result() const
+const VectorXd & QLD::result() const
 {
-	return X_;
+  return X_;
 }
 
-const VectorXd& QLD::multipliers() const
+const VectorXd & QLD::multipliers() const
 {
-	return U_;
+  return U_;
 }
 
 } // namespace Eigen
--- src/eigen-qld/QLD.h
+++ src/eigen-qld/QLD.h
@@ -17,123 +17,165 @@
 namespace Eigen
 {
 
-extern "C" int ql_(const int* m, const int* me, const int* mmax,
-	const int* n, const int* nmax, const int* mnn,
-	const double* c, const double* d, const double* a, const double* b,
-	const double* xl, const double* xu,
-	double* x, double* u, const double* eps, const int *mode,
-	const int* iout, int* ifail, const int* iprint,
-	double* war, int *lwar, int *iwar, int *liwar);
-
+extern "C" int ql_(const int * m,
+                   const int * me,
+                   const int * mmax,
+                   const int * n,
+                   const int * nmax,
+                   const int * mnn,
+                   const double * c,
+                   const double * d,
+                   const double * a,
+                   const double * b,
+                   const double * xl,
+                   const double * xu,
+                   double * x,
+                   double * u,
+                   const double * eps,
+                   const int * mode,
+                   const int * iout,
+                   int * ifail,
+                   const int * iprint,
+                   double * war,
+                   int * lwar,
+                   int * iwar,
+                   int * liwar);
 
 class QLD
 {
 public:
-	EIGEN_QLD_API QLD();
-	EIGEN_QLD_API QLD(int nrvar, int nreq, int nrineq, bool verbose=false);
+  EIGEN_QLD_API QLD();
+  EIGEN_QLD_API QLD(int nrvar, int nreq, int nrineq, bool verbose = false);
 
-	EIGEN_QLD_API void fdOut(int fd);
-	EIGEN_QLD_API int fdOut() const;
+  EIGEN_QLD_API void fdOut(int fd);
+  EIGEN_QLD_API int fdOut() const;
 
-	EIGEN_QLD_API void verbose(bool v);
-	EIGEN_QLD_API bool verbose() const;
+  EIGEN_QLD_API void verbose(bool v);
+  EIGEN_QLD_API bool verbose() const;
 
-	EIGEN_QLD_API int fail() const;
+  EIGEN_QLD_API int fail() const;
 
-	EIGEN_QLD_API void problem(int nrvar, int nreq, int nrineq);
+  EIGEN_QLD_API void problem(int nrvar, int nreq, int nrineq);
 
-	EIGEN_QLD_API const VectorXd& result() const;
+  EIGEN_QLD_API const VectorXd & result() const;
 
-	/** Return the lagrange multipliers associated with results **/
-	EIGEN_QLD_API const VectorXd& multipliers() const;
+  /** Return the lagrange multipliers associated with results **/
+  EIGEN_QLD_API const VectorXd & multipliers() const;
 
-	template <typename MatObj, typename VecObj,
-						typename MatEq, typename VecEq,
-						typename MatIneq, typename VecIneq,
-						typename VecVar>
-	bool solve(const MatrixBase<MatObj>& Q, const MatrixBase<VecObj>& C,
-		const MatrixBase<MatEq>& Aeq, const MatrixBase<VecEq>& Beq,
-		const MatrixBase<MatIneq>& Aineq, const MatrixBase<VecIneq>& Bineq,
-		const MatrixBase<VecVar>& XL, const MatrixBase<VecVar>& XU,
-		bool isDecomp=false, double eps=1e-12);
+  template<typename MatObj,
+           typename VecObj,
+           typename MatEq,
+           typename VecEq,
+           typename MatIneq,
+           typename VecIneq,
+           typename VecVar>
+  bool solve(const MatrixBase<MatObj> & Q,
+             const MatrixBase<VecObj> & C,
+             const MatrixBase<MatEq> & Aeq,
+             const MatrixBase<VecEq> & Beq,
+             const MatrixBase<MatIneq> & Aineq,
+             const MatrixBase<VecIneq> & Bineq,
+             const MatrixBase<VecVar> & XL,
+             const MatrixBase<VecVar> & XU,
+             bool isDecomp = false,
+             double eps = 1e-12);
 
 private:
-	MatrixXd A_;
-	VectorXd B_, X_;
-	int fdOut_;
-	int verbose_;
-	int fail_;
-	VectorXd U_;
-	VectorXd WAR_;
+  MatrixXd A_;
+  VectorXd B_, X_;
+  int fdOut_;
+  int verbose_;
+  int fail_;
+  VectorXd U_;
+  VectorXd WAR_;
   VectorXi IWAR_;
-  EIGEN_QLD_API inline int fortran_ql(const int* m, const int* me, const int* mmax,
-      const int* n, const int* nmax, const int* mnn,
-      const double* c, const double* d, const double* a, const double* b,
-      const double* xl, const double* xu,
-      double* x, double* u, const double* eps, const int *mode,
-      const int* iout, int* ifail, const int* iprint,
-      double* war, int *lwar, int *iwar, int *liwar)
+  EIGEN_QLD_API inline int fortran_ql(const int * m,
+                                      const int * me,
+                                      const int * mmax,
+                                      const int * n,
+                                      const int * nmax,
+                                      const int * mnn,
+                                      const double * c,
+                                      const double * d,
+                                      const double * a,
+                                      const double * b,
+                                      const double * xl,
+                                      const double * xu,
+                                      double * x,
+                                      double * u,
+                                      const double * eps,
+                                      const int * mode,
+                                      const int * iout,
+                                      int * ifail,
+                                      const int * iprint,
+                                      double * war,
+                                      int * lwar,
+                                      int * iwar,
+                                      int * liwar)
   {
-    return ql_(m, me, mmax, n, nmax, mnn, c, d, a, b, xl, xu, x, u, eps, mode, iout, ifail, iprint, war, lwar, iwar, liwar);
+    return ql_(m, me, mmax, n, nmax, mnn, c, d, a, b, xl, xu, x, u, eps, mode, iout, ifail, iprint, war, lwar, iwar,
+               liwar);
   }
 };
 
-
 // inline
 
-
-template <typename MatObj, typename VecObj,
-					typename MatEq, typename VecEq,
-					typename MatIneq, typename VecIneq,
-					typename VecVar>
-inline bool QLD::solve(const MatrixBase<MatObj>& Q, const MatrixBase<VecObj>& C,
-	const MatrixBase<MatEq>& Aeq, const MatrixBase<VecEq>& Beq,
-	const MatrixBase<MatIneq>& Aineq, const MatrixBase<VecIneq>& Bineq,
-	const MatrixBase<VecVar>& XL, const MatrixBase<VecVar>& XU,
-	bool isDecomp, double eps)
+template<typename MatObj,
+         typename VecObj,
+         typename MatEq,
+         typename VecEq,
+         typename MatIneq,
+         typename VecIneq,
+         typename VecVar>
+inline bool QLD::solve(const MatrixBase<MatObj> & Q,
+                       const MatrixBase<VecObj> & C,
+                       const MatrixBase<MatEq> & Aeq,
+                       const MatrixBase<VecEq> & Beq,
+                       const MatrixBase<MatIneq> & Aineq,
+                       const MatrixBase<VecIneq> & Bineq,
+                       const MatrixBase<VecVar> & XL,
+                       const MatrixBase<VecVar> & XU,
+                       bool isDecomp,
+                       double eps)
 {
-	assert(Aeq.rows() == Beq.rows()); // check equality size
-	assert(Aeq.cols() == X_.rows());
-	assert(Aineq.rows() == Bineq.rows()); // check inequality size
-	assert(Aineq.cols() == X_.rows());
-	assert(Q.rows() == Q.cols()); // check Q is square
-	assert(Q.cols() == X_.rows()); // check Q has the good number of variable
-	assert(C.rows() == X_.rows()); // check C size
-	assert(XU.rows() == X_.rows()); // check XL size
-	assert(XL.rows() == X_.rows()); // check XU size
+  assert(Aeq.rows() == Beq.rows()); // check equality size
+  assert(Aeq.cols() == X_.rows());
+  assert(Aineq.rows() == Bineq.rows()); // check inequality size
+  assert(Aineq.cols() == X_.rows());
+  assert(Q.rows() == Q.cols()); // check Q is square
+  assert(Q.cols() == X_.rows()); // check Q has the good number of variable
+  assert(C.rows() == X_.rows()); // check C size
+  assert(XU.rows() == X_.rows()); // check XL size
+  assert(XL.rows() == X_.rows()); // check XU size
 
-	int nreq = int(Beq.rows());
-	int nrineq = int(Bineq.rows());
-	int nrvar = int(X_.rows());
+  int nreq = int(Beq.rows());
+  int nrineq = int(Bineq.rows());
+  int nrvar = int(X_.rows());
 
-	int mode = isDecomp ? 0 : 1;
+  int mode = isDecomp ? 0 : 1;
 
-	int M = nreq + nrineq;
-	int N = nrvar;
+  int M = nreq + nrineq;
+  int N = nrvar;
 
-	int MMAX = int(A_.rows());
-	// beware, don't work if base Q is not square
-	int NMAX = int(Q.rows());
+  int MMAX = int(A_.rows());
+  // beware, don't work if base Q is not square
+  int NMAX = int(Q.rows());
 
-	int NMN = M + 2*N;
-	int LWAR = int(WAR_.rows());
-	int LIWAR = int(IWAR_.rows());
+  int NMN = M + 2 * N;
+  int LWAR = int(WAR_.rows());
+  int LIWAR = int(IWAR_.rows());
 
-	A_.block(0, 0, nreq, nrvar) = -Aeq;
-	A_.block(nreq, 0, nrineq, nrvar) = -Aineq;
+  A_.block(0, 0, nreq, nrvar) = -Aeq;
+  A_.block(nreq, 0, nrineq, nrvar) = -Aineq;
 
-	B_.segment(0, nreq) = Beq;
-	B_.segment(nreq, nrineq) = Bineq;
+  B_.segment(0, nreq) = Beq;
+  B_.segment(nreq, nrineq) = Bineq;
 
-	fortran_ql(&M, &nreq, &MMAX, &N, &NMAX, &NMN,
-		Q.derived().data(), C.derived().data(), A_.data(), B_.data(),
-		XL.derived().data(), XU.derived().data(), X_.data(),
-		U_.data(), &eps, &mode, &fdOut_, &fail_, &verbose_,
-		WAR_.data(), &LWAR, IWAR_.data(), &LIWAR);
+  fortran_ql(&M, &nreq, &MMAX, &N, &NMAX, &NMN, Q.derived().data(), C.derived().data(), A_.data(), B_.data(),
+             XL.derived().data(), XU.derived().data(), X_.data(), U_.data(), &eps, &mode, &fdOut_, &fail_, &verbose_,
+             WAR_.data(), &LWAR, IWAR_.data(), &LIWAR);
 
-	return fail_ == 0;
+  return fail_ == 0;
 }
 
-
 } // namespace Eigen
-
